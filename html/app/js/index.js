317b7a3c3a6c9e8a9724a450853a82a7e8cbf7ba
Andriy Kondratyuk <gvakon@gmail.com>
2019-12-10 00:27:19 +0200

    +catalog list +advants +promo

--- html/app/js/index.js
+++ html/app/js/index.js
@@ -9,22 +9,7 @@ var Home = function () {
 				slidesToScroll: 1,
 				autoplay: true,
 				arrows: true,
-				dota: true,
-				prevArrow: '<a class="slick-prev"><i class="fa fa-angle-left"></i></a>',
-				nextArrow: '<a class="slick-next"><i class="fa fa-angle-right"></i></a>',
-				adaptiveHeight: true
-			});
-		}
-	};
-
-	var bbSlider = function () {
-		var sl = $('.bb-list');
-		if (sl.length) {
-			sl.slick({
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				autoplay: true,
-				arrows: true,
+				dots: true,
 				prevArrow: '<a class="slick-prev"><i class="fa fa-angle-left"></i></a>',
 				nextArrow: '<a class="slick-next"><i class="fa fa-angle-right"></i></a>',
 				adaptiveHeight: true
