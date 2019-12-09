49149efe1d4a1d151b4d995317dfa02a22e46e62
Andriy Kondartyuk <gvakon@gmail.com>
2019-12-09 17:20:33 +0200

    header+footer+slider

--- html/app/js/index.js
+++ html/app/js/index.js
@@ -5,11 +5,11 @@ var Home = function () {
 		var sl = $('.sl');
 		if (sl.length) {
 			sl.slick({
-				fade: true,
 				slidesToShow: 1,
 				slidesToScroll: 1,
 				autoplay: true,
 				arrows: true,
+				dota: true,
 				prevArrow: '<a class="slick-prev"><i class="fa fa-angle-left"></i></a>',
 				nextArrow: '<a class="slick-next"><i class="fa fa-angle-right"></i></a>',
 				adaptiveHeight: true
