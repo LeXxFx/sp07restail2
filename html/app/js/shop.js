c65b8270b635f5e825c5590d2978e8ac0ad4facf
Andriy Kondratyuk <gvakon@gmail.com>
2020-03-18 02:53:27 +0200

    add compare lis style

--- html/app/js/shop.js
+++ html/app/js/shop.js
@@ -304,15 +304,6 @@ var Shop = function () {
 		}
 	}
 
-	var brands = function () {
-		var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
-
-		var arr = document.getElementsByClassName("autocomplete");
-		[].forEach.call(arr, function (el) {
-			autocomplete(el.querySelector('.form-control'), countries);
-		});
-	}
-
 	var cart = function () {
 		$('.cart-item__options').on('click', '.prop-change', function (e) {
 			e.preventDefault();
@@ -343,6 +334,61 @@ var Shop = function () {
 		});
 	}
 
+	var compareList = function () {
+		var $page = $('.compare'),
+			$cursor = $('.compare-cursor'),
+			$products = $('.compare-list .compare-item');
+
+		if ($products.length > 0) {
+			$page.addClass('lazy-load');
+			$products.each(function () {
+				var $item = $(this);
+				$item.find(".compare-values .value").each(function () {
+					var that = $(this),
+						maxHeight = that.height(),
+						h = that.css('height', 'auto').height(),
+						i = that.index();
+
+					maxHeight = maxHeight > h ? maxHeight : h;
+
+					$('.val-' + i).height(maxHeight);
+					$(".compare-side .value:eq(" + i + ")").height(maxHeight);
+				});
+			}).promise().done(function () {
+				const ps = new PerfectScrollbar('.compare-list', {
+					wheelSpeed: 2,
+					wheelPropagation: true,
+					minScrollbarLength: 20,
+					suppressScrollY: true,
+					useBothWheelAxes: true
+				});
+				ps.update();
+
+				setTimeout(function () {
+					$page.removeClass('lazy-load').addClass('loaded');
+				}, 500);
+			});
+
+			$('.compare-values .value').mouseenter(function () {
+				var that = $(this),
+					indx = that.index(),
+					$vals = $('.val-' + indx),
+					self = $vals[1],
+					h = self.offsetHeight;
+
+				$vals.addClass('active');
+				$cursor.addClass('active').css({
+					'height': h,
+					'top': self.offsetTop + 1
+				});
+
+			}).mouseleave(function () {
+				$('.compare-values .value.active').removeClass('active');
+				$cursor.removeClass('active');
+			});
+		}
+	}
+
 	return {
 		init: function () {
 			inputCounter();
@@ -352,9 +398,9 @@ var Shop = function () {
 			productGallery();
 			productFilters();
 			productSingle();
-			brands();
 			cart();
 			wishList();
+			compareList();
 		}
 	};
 }();
